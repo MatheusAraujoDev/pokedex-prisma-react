3ac7732d986075072dee91480ebe90adf038a144
Matheus <matheusaraujo.dev@gmail.com>
2023-04-19 13:38:13 -0300

    Style: css mobile improviments

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './dashboard.css';
 import { Datatable } from '../../components/Table';
+import PokemonTitle from '../../assets/pokedex_list_title.png'
 
 export default function PokemonList() {
 
   return (
     <div className='pokedex-page-container'>
-      <p className='pokedex-title'>Pokémon List</p>
+      <img className='pokedex-title' src={PokemonTitle} />
       <div className="pokedex-table-container">
         <Datatable />
       </div>
